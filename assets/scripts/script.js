ecaef374257236631eff469ab26a25a453ef866f
Abe <abe.wav.bis@gmail.com>
2023-10-18 15:36:19 -0500

    Updated HTML + CSS, Built right side of user interface

---
+++ assets/scripts/script.js
@@ -0,0 +1,12 @@
+// To do:
+
+// Add function to use textinput value to make an API call to grab weather instanceof
+
+// Add function that updates html elements with API Response
+
+// Add function that saves and renders search inputs to the history container
+
+// Add function for when a user clicks on a history item it updates data to that city
+
+
+
